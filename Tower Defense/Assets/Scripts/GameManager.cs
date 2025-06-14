2a40dbff5bee7252ac5d7b986f5fc2214e499b9a
VivJRC <127207570+VivJRC@users.noreply.github.com>
2025-06-14 22:42:28 +0200

    speed button

--- Tower Defense/Assets/Scripts/GameManager.cs
+++ Tower Defense/Assets/Scripts/GameManager.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using Terrain;
 using Enemies;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private EnemyManager _enemyManager;
     [SerializeField] private MapManager _mapView;
     [SerializeField] private WaveManager _waveManager;
+    [SerializeField] private Button _speedBtn;
+
+    private float _speed;
 
     private void Start()
     {
@@ -17,11 +21,23 @@ public class GameManager : MonoBehaviour
         List<Cell> path = _mapView.GetPath();
         _enemyManager.Init(start, path);
         _waveManager.Init();
+        _speed = 1f;
+        _speedBtn.onClick.AddListener(OnSpeedBtnClicked);
+    }
+
+    private void OnDestroy()
+    {
+        _speedBtn.onClick.RemoveListener(OnSpeedBtnClicked);
+    }
+
+    private void OnSpeedBtnClicked()
+    {
+        _speed = (_speed == 1f) ? 3f : 1f;
     }
 
     private void Update()
     {
-        float deltaTime = Time.deltaTime;
+        float deltaTime = Time.deltaTime * _speed;
         _waveManager.CustomUpdate(deltaTime);
         if (_waveManager.frameSpawn.Count > 0)
         {
